a95bb3a206ac86b5cb52dd84b91f4cd85a2d2d31
adejumoridwan <you@example.com>
2022-07-17 16:36:57 +0100

    folder assignment

--- angle_at_angle.py
+++
@@ -1,7 +0,0 @@
-from turtle import *
-
-for angle in range(0, 360, 15):
-    setheading(angle)
-    forward(100)
-    write(str(angle) + '�')
-    backward(100)
---
+++ basic/angle_at_angle.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+for angle in range(0, 360, 15):
+    setheading(angle)
+    forward(100)
+    write(str(angle) + '😎')
+    backward(100)
+
+    list1 = 30
+    list2 = 30
+
+    list1 == list2
+    list1 is list2
+
+    sample = ["john"]
+
+    sample.append(2,"f")
+
+    print("hee"*2)
